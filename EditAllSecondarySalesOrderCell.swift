f662d9314828392cd38a6014977b425ae32a18a9
FURKAN VIJAPURA <frkgraphic@gmail.com>
2017-11-15 14:55:02 +0530

    Update EditAllSecondarySalesOrderCell.swift

--- EditAllSecondarySalesOrderCell.swift
+++ EditAllSecondarySalesOrderCell.swift
@@ -1,12 +1,7 @@
-//
-//  EditAllSecondarySalesOrderCellTableViewCell.swift
-//  Odin_App_Project_Swift
-//
-//  Created by discussolutions on 9/19/17.
-//  Copyright © 2017 discussolutions. All rights reserved.
-//
+
 
 import UIKit
+
 // 1 >Create protocolo 
 protocol MyCellDelegate {
     func btnCloseTapped(cell: EditAllSecondarySalesOrderCell)
@@ -14,16 +9,14 @@ protocol MyCellDelegate {
 
 class EditAllSecondarySalesOrderCell: UITableViewCell{
 
-    @IBOutlet var mainView: UIView!
-    @IBOutlet var txtCart: RoundTextField!
-    @IBOutlet var txtRateValue: RoundTextField!
-    @IBOutlet var txtProductQuantity: RoundTextField!
-    @IBOutlet var txtProductName: RoundTextField!
+  
+    
      var delegate: MyCellDelegate? // 6 > add your proto in var
     override func awakeFromNib() {
         super.awakeFromNib()
       
     }
+    
     // 7 > crate your button action in tableview cell for
     @IBAction func btnDeleteTapped(_ sender: Any)
     {
@@ -32,6 +25,7 @@ class EditAllSecondarySalesOrderCell: UITableViewCell{
             delegate?.btnCloseTapped(cell: self)
         }
     }
+    
     override func setSelected(_ selected: Bool, animated: Bool)
     {
         super.setSelected(selected, animated: animated)
